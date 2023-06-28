05baa6b8132d3981af9a0f39c337480c56f6c71e
Michal Smyrnow <dsw49561@student.dsw.edu.pl>
2023-06-28 23:35:14 +0200

    refactored script for data displaying

--- data.sh
+++ data.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Pobieranie dzisiejszej daty
-current_date=$(date +"%Y-%m-%d")
+if [[ $1 == "--date" ]]; then
+  # Pobieranie dzisiejszej daty
+  current_date=$(date +"%Y-%m-%d")
 
-# Wyświetlanie dzisiejszej daty
-echo "Dzisiejsza data: $current_date"
+  # Wyświetlanie dzisiejszej daty
+  echo "Dzisiejsza data: $current_date"
+else
+  echo "Użycie: skrypt.sh --date"
+  exit 1
+fi
